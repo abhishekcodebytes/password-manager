3e5de40045e4ac80482c39d334bdb8a886cc1574
abhishekcodebytes <mrabhishekkumar25@gmail.com>
2026-09-11 09:26:30 +0530

    Fixed frontend issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,20 +8,11 @@ const Navbar = () => {
     return (
         <nav className='bg-amber-500 
         flex justify-between items-center h-20 px-5 py-3'>
-            <div className='logo font-bold'>trial</div>
+            <div className='logo font-bold'>Save Your Password</div>
             <ul>
-                <li className=' flex gap-4'>
-                    <a className='hover:font-bold' href="">Home</a>
-                    <a className='hover:font-bold' href="">About</a>
-                    <a className='hover:font-bold' href="">Contact</a>
-
-                </li>
+                
             </ul>
-           <button className='text-black bg-green-500  my-5 rounded-md flex gap-2
-           justify-center items-center '>
-                <img  className = 'invert py-1 w-10'  src="public/github.png" alt="" />
-                <span className='font-bold p-1 '>github</span>
-            </button>
+          
         </nav>
     )
 }
